d56d13c0fc57f8fb4ef9bda59b57dd97e42a83d1
X-XSpeedBlack <isaquesousa777@gmail.com>
2021-02-27 15:43:39 -0300

    first commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
+import styles from "../styles/components/Profile.module.css";
+
+export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/X-XSpeedBlack.png" alt="Isaque.S" />
+
+      <div>
+        <strong>Isaque de Sousa</strong>
+
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level {level}
+        </p>
+      </div>
+    </div>
+  );
+}
